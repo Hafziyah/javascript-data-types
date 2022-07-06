dfe1b4c2f1209ede4000026ebeaef7aff169d0fa
Hafsat Ajia-Egbeyemi <hafsatajia@gmail.com>
2022-07-06 12:12:46 +0100

    Corrected previous errors and simplified the file.

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 let firstQuestion = prompt ("Do you know what JavaScript data types are? True or False");
-let followUp =prompt ("Are they Booleans, Strings, Numbers, Null and Undefined?");
-let answer = prompt ("There are 5 types of basic or primitive data types ie Booleans (True/False), Numbers (-1, 0, 1, 1.50), Strings ('Loaves', 'Peter Pan', 'etc'), Null and undefined");
 
-alert (firstQuestion)
 
 if (firstQuestion ==="True") {
-    alert ("Good!")
-} else if (followUp ==="Yes") {
-    alert("Great") 
-} else {
-    alert (`${answer}, you need to learn more about data types because those are the JavaScript data types.`);
+    alert ("Great! So you know JavaScript data types are Booleans, Strings, Numbers, Null and Undefined.");
+} else if (firstQuestion ==="False") {
+    alert("There are 5 types of basic or primitive data types in JavaScript, they are Booleans, Strings, Numbers, Null and Undefined?");
+}else{
+    alert ("You need to find out more about JavaScript Data Types")
 };
